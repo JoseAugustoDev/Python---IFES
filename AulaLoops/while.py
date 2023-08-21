e76a9680e87d4f68282583d4193d42746a4d68a6
Jose Augusto Costa <joseaugustodeveloper@gmail.com>
2023-08-21 19:04:18 -0300

    loops

---
+++ AulaLoops/while.py
@@ -0,0 +1,25 @@
+i = 1
+
+while i < 6:
+    print(i)
+    i += 1
+
+############
+
+j = 1
+
+while j < 10:
+    print(j)
+
+    if j == 5:
+        break
+
+    j += 1
+
+fruits = ['Maçã', 'Amora', 'Laranja']
+
+for x in fruits:
+    print(x)
+
+for x in 'paralelepípedo':
+    print(x)
