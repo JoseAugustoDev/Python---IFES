5274ea13756ee5aaafe0ec0ab4070137703b592f
Jose Augusto Costa <75041668+JoseAugustoDev@users.noreply.github.com>
2023-09-11 19:11:55 -0300

    change

---
+++ AulaListas/exemplo.py
@@ -0,0 +1,23 @@
+numeros = [1, 2, 3, 4, 5]
+
+vazia = []
+
+for x in numeros: 
+    print(x)
+
+'''for y in range(0,10): 
+    vazia.append(y)
+    print(vazia)
+        if y % 2 == 0:
+        vazia.remove(y)
+        print(vazia)'''
+
+frutas = ["maçã", "abacaxi", "laranja", "banana"]
+
+outras_frutas = ["pêra", "maçã"]
+
+todas_as_frutas = [frutas + outras_frutas]
+
+sub_listas = numeros[1:4]
+
+print(sub_listas)
