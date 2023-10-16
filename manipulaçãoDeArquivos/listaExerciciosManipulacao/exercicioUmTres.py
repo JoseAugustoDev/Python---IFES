80fc2fcbb9c195a4c2c6238dc0e72d19c2cf06f2
Jose Augusto Costa <75041668+JoseAugustoDev@users.noreply.github.com>
2023-10-16 20:12:53 -0300

    Real Avanço

--- manipulaçãoDeArquivos/listaExerciciosManipulacao/exercicioUmTres.py
+++ manipulaçãoDeArquivos/listaExerciciosManipulacao/exercicioUmTres.py
@@ -1,18 +1,29 @@
-with open('manipulaçãoDeArquivos/listaExerciciosManipulacao/dicionarioEstudantes.txt', 'w') as lista:
-    resp = 1
+def ler_dados():
 
-    estudante = {}
-    
-    while resp != 0:
-        nome_estudante = input("Digite o nome do estudante: ")
-        idade_estudante = input("Digite a idade do estudante: ")
-        curso_estudante = input("Digite o curso do estudante: ")
+    nome_estudante = input("Digite o nome do estudante: ")
+    idade_estudante = int(input("Digite a idade do estudante: "))
+    curso_estudante = input("Digite o curso do estudante: ")
 
-        estudante['nome'] = nome_estudante
-        estudante['idade'] = idade_estudante
-        estudante['curso'] = curso_estudante
+    estudante = {
+        "nome": nome_estudante,
+        "idade": idade_estudante,
+        "curso": curso_estudante,
+    }
 
-        lista.write(f"{estudante}\n")
+    return estudante
 
-        resp = int(input("Digite 1 se deseja continuar: \nDigite 0 se deseja parar: \n"))
+def criar_arquivo(dados_aluno):
 
+    with open('manipulaçãoDeArquivos/listaExerciciosManipulacao/dicionarioEstudantes.txt', 'w') as lista:
+
+        lista.write(f"{dados_aluno}\n")
+
+def ler_arquivo():
+    with open('manipulaçãoDeArquivos/listaExerciciosManipulacao/dicionarioEstudantes.txt', 'r') as lista:
+        for linha in lista:
+            print(linha)
+
+if __name__ == '__main__':
+    dados_alunos = ler_dados()
+    criar_arquivo(dados_aluno=dados_alunos)
+    ler_arquivo()
